b53f4f2d59e792a3968372aad35bebbe936ac163
marti <mart2004xd@gmail.com>
2025-05-16 16:02:48 +0300

    can solve image questions now

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ const  {answerQuestion} = require('./groq.js');
   });
   const page = await browser.newPage();
 
-  await page.goto('https://mdl.fcst.tu-sofia.bg/mod/quiz/view.php?id=2629', {waitUntil : 'networkidle2'});
+  await page.goto('https://mdl.fcst.tu-sofia.bg/mod/quiz/view.php?id=361', {waitUntil : 'networkidle2'});
   console.log("Page loaded\n");
 
   let login = await moodle_login(process.env.MOODLE_USERNAME ,  process.env.MOODLE_PASSWORD , page);
@@ -23,23 +23,16 @@ const  {answerQuestion} = require('./groq.js');
   await delay(1000)
 
 
-  console.log("running quiz auth check :)")
-  // await delay(3000)
-  await quizAuth(page);
+  
 
   await page.waitForSelector("div.singlebutton.quizstartbuttondiv", {timeout : 3000})
   await page.click("div.singlebutton.quizstartbuttondiv")
   console.log("quiz start button clicked")
-  
-  // await delay(1000)
 
-  // await page.waitForSelector('input#id_cancel[type="submit"][value="Отказване"]')
-  // await page.click('input#id_cancel[type="submit"][value="Отказване"]')
-
+  console.log("running quiz auth check :)")
+  await quizAuth(page, "123456");
 
   
-  
-
   //! calling delay function before load event prevents it from happening
 
   page.on("load",async function handleLoad(){
@@ -51,18 +44,23 @@ const  {answerQuestion} = require('./groq.js');
     if(questionElement){
       console.log("question element found, getting answer...\n")
 
-      //put all question data in json
+      //? put all question data in json
       let questionData = {};
       await getQuestionData(page, questionData);
+      
+      //? pass answer and question type to submit function
 
-      //pass answer and question type to submit function
+      let {img_base64, ...qDataExtract} = questionData
+      console.log(qDataExtract);
+      console.log("----------------------------------------")
       let answer = await answerQuestion(questionData);
-      let qType  = questionData['questionType'];
-      // console.log(questionData);
-      // console.log(answer)
-      // console.log(`qType is ${qType}`)
+      // let qType  = 
+      console.log(answer)
+      console.log("----------------------------------------")
 
-      submitQuestion(page, answer, qType)
+      console.log("waiting 0.4s before submitting")
+      await delay(400)
+      submitQuestion(page, answer, questionData['questionType'])
     }
 
     else{
@@ -79,61 +77,5 @@ const  {answerQuestion} = require('./groq.js');
       return;
     }
   })
-
-
-
-  // page.on("load", async function handleLoad(){
-  //   console.log("page loading, waiting 1s\n")
-  //   await delay(1000)
-    
-  //   let questionElement = await page.$('div.qtext');
-
-  //   if(questionElement){
-  //     console.log("question element found\n")
-
-  //     let questionData = {};
-  //     await getQuestionData(page, questionData);
-
-
-  //     //handle answer for single choice question
-  //       let correct = await answerQuestion (questionData);
-
-  //       await page.evaluate((index) => {
-  //         let answers = document.querySelector("div.answer").children;
-  //         answers[index].querySelector('input[type="radio"]').click();
-  //       }, correct)
-  //     //ends here
-
-  //     console.log(`for question ${qCount} clicked on answer ${answer_index} \nwaiting 1s `)
-  //     qCount++;
-
-  //     await delay(1000)
-  //     await page.waitForSelector('div.submitbtns > input#mod_quiz-next-nav[type="submit"]')
-  //     await page.click('div.submitbtns > input#mod_quiz-next-nav[type="submit"]');
-
-  //   }
-
-  //   else{
-  //     console.log("no more questions :) \nsubmitting now!");
-  //     await page.waitForSelector('button.btn-primary[type="submit"]')
-  //     await page.click('button.btn-primary[type="submit"]')
-
-  //     await delay(500)
-
-  //     await page.waitForSelector('button.btn-primary[type="button"][data-action="save"]')
-
-
-  //     await page.click('button.btn-primary[type="button"][data-action="save"]')
-
-  //     page.off("load", handleLoad);
-  //     return;
-  //   }
-  // })
-
-
-
-
-  //TESTING CURRENTLY
-
 
 })();
