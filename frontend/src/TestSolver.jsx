5260477f2f07801c7204241098227ac0b9f09630
martiparti <99166596+martiparti1@users.noreply.github.com>
2025-06-03 16:39:25 +0300

    send password aswell

--- frontend/src/TestSolver.jsx
+++ frontend/src/TestSolver.jsx
@@ -6,6 +6,7 @@ export default function TestSolver(){
 
     const [hasExample, setHasExample] = useState(true)
     const [isLoading, setIsLoading] = useState(false)
+    const [quizPassword, setQuizPassword] = useState("")
     const [quizLinkInput, setQuizLinkInput] = useState("");
     const [quizResult, setQuizResult] = useState("");
     const [err, setErr] = useState("")
@@ -19,7 +20,8 @@ export default function TestSolver(){
         try {
 
             const res = await axios.post("http://localhost:3000/start-quiz", {
-                quizUrl : quizLinkInput
+                quizUrl : quizLinkInput,
+                quizPassword : quizPassword
             });
 
             setQuizResult(res.data)
@@ -44,7 +46,7 @@ export default function TestSolver(){
         <>
             <title>Quiz submit</title>
             <div className="min-h-screen bg-gray-500 flex justify-center items-center">
-                <div className="min-h-[50dvh] min-w-[70dvw] bg-white rounded-lg shadow-black flex flex-col justify-center items-center">
+                <div className="min-h-[50dvh] min-w-[70dvw] bg-white rounded-lg shadow-black flex flex-col justify-center items-center p-[10px]">
                 
                     
 
@@ -65,7 +67,7 @@ export default function TestSolver(){
                         </>
                     )}
 
-                    <div className={`flex flex-row justify-between items-center min-w-[50dvw]`}>
+                    <div className={`flex flex-col justify-between items-center gap-[10px] min-w-[50dvw]`}>
                         <input 
                             type="text" 
                             className="input-m w-[30dvw]"
@@ -73,9 +75,17 @@ export default function TestSolver(){
                             disabled = {isLoading}
                             onChange={(e)=>setQuizLinkInput(e.target.value)}
                         />
+
+                        <input 
+                            type="text" 
+                            className="input-m w-[30dvw]"
+                            placeholder="quiz password here (leave empty if no password)" 
+                            disabled = {isLoading}
+                            onChange={(e)=>setQuizPassword(e.target.value)}
+                        />
                         <button
                             type = "button"
-                            className = {`min-w-[15dvw] min-h-[4dvh]  text-white font-bold rounded-lg cursor-pointer  ${isLoading ? `bg-orange-400 ` : `bg-green-500 active:bg-green-600 active:scale-95`}`} 
+                            className = {`min-w-[15dvw] min-h-[4dvh]  text-white font-bold rounded-lg cursor-pointer ${isLoading ? `bg-orange-400 ` : `bg-green-500 active:bg-green-600 active:scale-95`}`} 
                             // onClick={()=> {setHasExample(false) ; setIsLoading(true)}}
                             onClick={handleClick}
                             disabled = {isLoading}
