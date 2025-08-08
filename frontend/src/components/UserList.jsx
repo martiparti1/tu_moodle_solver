b131b25c4577699e8fceaa6a989d36ded274bc82
martiparti <99166596+martiparti1@users.noreply.github.com>
2025-08-08 16:50:33 +0300

    can toggle account_special

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -10,6 +10,16 @@ export default function UserList({users, setUsers}){
         setUsers(prev => prev.filter(user => user.id != delete_this))
     }
 
+    async function handleToggleSpecialClick(e){
+        let toggle_this = e.currentTarget.closest('div[data-id]')?.dataset.id;
+
+        await axios.post('http://localhost:3000/toggle-special', {user_id : toggle_this});
+
+        setUsers(prev => prev.map(user => 
+            user.id = toggle_this ? {...user, isSpecial : !isSpecial} : user
+        ))
+    }
+
     return(
         <div className = "flex flex-col">
             {users.map((user) => 
@@ -19,8 +29,8 @@ export default function UserList({users, setUsers}){
                         
                         <p><b>Username : </b> {user.username}</p>
                         <button 
-                        className="bg-red-400 text-white rounded-lg px-[7px] active:scale-95 min-h-[30px]"
-                        onClick = {handleDeleteClick}
+                            className="bg-red-400 text-white rounded-lg px-[7px] active:scale-95 min-h-[30px]"
+                            onClick = {handleDeleteClick}
                         ><b>DELETE THIS GUY</b></button>
                     </div>
                     <p>Email : {user.email != null ? user.email : "none"}</p>
@@ -32,7 +42,12 @@ export default function UserList({users, setUsers}){
 
                     <div className="flex flex-row justify-between">
                         <p>Special : {user.special ? <span className="text-green-500 font-bold" >YES</span> : <span className="text-red-500 font-bold"> NO </span>}</p>
-                        <button>toggle special</button>
+                        <button
+                            className="bg-cyan-400 text-white rounded-lg px-[7px] active:scale-95 min-h-[30px]"
+                            onClick={handleToggleSpecialClick}
+                        >
+                            toggle special
+                        </button>
                     </div>
                 </div>
             )}
