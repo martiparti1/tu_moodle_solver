a53dea5565dcd04023072d3a09678b430f105f15
martiparti <99166596+martiparti1@users.noreply.github.com>
2025-08-08 12:07:00 +0300

    fix browser not opening lol

--- backend/solveQuiz_v2.js
+++ backend/solveQuiz_v2.js
@@ -6,8 +6,13 @@ const {getBrowser} = require('./util/server_lib.js');
 
 async function solveQuiz(quiz_url, quiz_password, user_email, user_password){
   const browser = await getBrowser();
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
+  // const browser = await puppeteer.launch({
+  //             headless : false,
+  //             executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+  //             defaultViewport : null
+  //         })
+  // const context = await browser.createIncognitoBrowserContext();
+  const page = await browser.newPage();
 
   await page.goto(quiz_url, {waitUntil : 'networkidle2'});
   console.log("Page loaded\n");
@@ -18,28 +23,25 @@ async function solveQuiz(quiz_url, quiz_password, user_email, user_password){
 
   await start_auth(page, quiz_password)
 
-  // let retry_counter = 0;
   while(true){
+
     try{ 
       //wait for document to be ready AND give .3s buffer to prevent loading fuckups
       await page.waitForFunction(() => document.readyState === 'complete');
       await delay(300);
       await page.waitForSelector('.qtext', {timeout : 5000, visible : true})
-    } catch (err) {
-      
-      //sometimes there are loading errors so run the check a couple of times to confirm if there really isnt a question
-      // if(retry_counter++ >= 3) { 
-      //   console.log("4 fuckups reached. you probably have to submit by now or have err ::DDDDDD");
-        
-        console.log(err)
-        break; 
-      // }
-      // console.log("qtext loading fuck up ,, waiting .3s and retrying")
-      // await delay(300);
-      // continue;
-    }
+    } catch(err){
 
-    retry_counter = 0;
+      if(err.name == "TimeoutError"){
+        console.log("No question found ;; going to submit now!!!!!");
+        break;
+      }
+      else{ 
+        console.log(err)
+        break;
+      } 
+
+    }
     let questionData = {};
     await getQuestionData(page, questionData);
 
@@ -74,9 +76,10 @@ async function solveQuiz(quiz_url, quiz_password, user_email, user_password){
 
   console.log("CLICKED SECOND SUBMIT BUTTON // CLOSING BROWSER")
   await page.close();
-  await context.close();
+  await browser.close();
 }
 module.exports = {solveQuiz}
 
 
-// solveQuiz("https://e-edu.nbu.bg/mod/quiz/view.php?id=1398814", '')
+// solveQuiz("https://mdl.fcst.tu-sofia.bg/mod/quiz/view.php?id=2629", '')
+// solveQuiz('https://mdl.fcst.tu-sofia.bg/mod/quiz/view.php?id=2629', '', 'martinrpetrov' , 'martinpetrovTU7!')
